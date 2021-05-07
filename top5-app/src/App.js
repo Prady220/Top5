a32d9ad3d9d190dbef1310e743eeabea88d79c87
Prady220 <pradeep.ece.gitam@gmail.com>
2021-05-07 02:05:57 -0500

    Minor refactoring and tinkering

--- top5-app/src/App.js
+++ top5-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Yelp from './util/yelp.js'
+import Yelp from './util/yelp.js';
 import React, { useState, useEffect } from 'react';
 import BusinessView from './components/BusinessView.js';
 import SearchBar from './components/SearchBar.js';
@@ -35,21 +35,18 @@ export default function App() {
       setLoading(true);
       const businessesList = await Yelp.searchInYelp(term, location, limit);
       const businessesResolved = [];
-      businessesList.map(
-        async business =>
-          await business.then(value => businessesResolved.push(value))
+      businessesList.map(business =>
+        business.then(value => businessesResolved.push(value))
       );
       setBusinesses(businessesResolved);
       setTimeout(() => {
         setLoading(false);
-      }, 6000);
+      }, 5000);
     } catch (error) {
-      setError("Bad request");
+      setError('Bad request');
       setLoading(false);
     }
   }
-
-  const isLoading = (loading) ? <h2>Loading...</h2> : null;
 
   let businessesElements = businesses.map((business, i) => (
     <BusinessView key={i} business={business} />
@@ -64,18 +61,16 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SearchBar 
+        <SearchBar
           searchInYelp={searchInYelp}
           handleLimitChange={handleLimitChange}
           handleLocationChange={handleLocationChange}
           handleTermChange={handleTermChange}
         />
-        {isLoading}
-        {errors}
         <h2>
           Top {limit} places to get {term} in {location}
         </h2>
-        { businessesElements }
+        {loading ? <h2>Loading...</h2> : [businessesElements]}
       </header>
     </div>
   );
