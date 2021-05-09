693b4c4403b13406833a5afba299810e8b108033
Prady220 <pradeep.ece.gitam@gmail.com>
2021-05-09 03:08:59 -0500

    Stored the API_KEY in the environment variable

--- top5-app/src/util/yelp.js
+++ top5-app/src/util/yelp.js
@@ -1,54 +1,60 @@
 // Got bearer apiKey from https://www.yelp.com/developers/v3/manage_app;
-const apiKey = 'Bqw9DcGp31OLXAjRz93Rx8TqMDP6HiFQ0lxIRZMk6x53kzarAMFL8hQtatVzUCxSSSs0RvDSTJhOEO5z7IPSgqDilMifA6LliLZoGuwZRGgdUSxYGYknlk68hZGMYHYx';
+const apiKey = process.env.REACT_APP_YELP_API_KEY;
 const yelp = {
-    searchInYelp(term = 'icecream', location = 'alpharetta, ga', limit = 5) 
-    {
-        return fetch(`/businesses/search?term=${term}&location=${location}&sort_by=rating&limit=${limit}`, 
-        {       
-            headers: { 
-                Accept: "application/json",
-                Authorization: `Bearer ${apiKey}`
-            },
-        }).then( (res) => {
-            return res.json();
-        }).then(data => {
-            if(data.businesses) {
-                return data.businesses.map((business) => {
-                    return fetch(`/businesses/${business.id}/reviews`, 
-                    {       
-                        headers: { 
-                            Accept: "application/json",
-                            Authorization: `Bearer ${apiKey}`
-                        }
-                    }).then((res) => {
-                        return res.json()
-                    }).then(data => {
-                        if(data.reviews) {
-                            const review = data.reviews[0];
-                            const businessObj = {
-                                id: business.id,
-                                imageSrc: business.image_url,
-                                name: business.name,
-                                address: business.location.display_address.join(", "),
-                                phone: business.phone,
-                                rating: business.rating,
-                                reviewCount: business.review_count,
-                                reviewer: review.user.name,
-                                reviewerComment: review.text,
-                                reviewerRating: review.rating
-                            }
-                            return businessObj;
-                        }
-                    }).catch(error => {
-                        return error;
-                    })
-                })
-            }
-        }).catch(error => {
-            console.error(error);
-            return error;
-        })
-    }
-}
+  searchInYelp(term = 'icecream', location = 'alpharetta, ga', limit = 5) {
+    return fetch(
+      `/businesses/search?term=${term}&location=${location}&sort_by=rating&limit=${limit}`,
+      {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        if (data.businesses) {
+          return data.businesses.map(business => {
+            return fetch(`/businesses/${business.id}/reviews`, {
+              headers: {
+                Accept: 'application/json',
+                Authorization: `Bearer ${apiKey}`,
+              },
+            })
+              .then(res => {
+                return res.json();
+              })
+              .then(data => {
+                if (data.reviews) {
+                  const review = data.reviews[0];
+                  const businessObj = {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.display_address.join(', '),
+                    phone: business.phone,
+                    rating: business.rating,
+                    reviewCount: business.review_count,
+                    reviewer: review.user.name,
+                    reviewerComment: review.text,
+                    reviewerRating: review.rating,
+                  };
+                  return businessObj;
+                }
+              })
+              .catch(error => {
+                return error;
+              });
+          });
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        return error;
+      });
+  },
+};
 
 export default yelp;
